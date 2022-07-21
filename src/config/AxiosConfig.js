73d25a41933e31d174e08dd50359393091cab902
caceres1992 <caceresvillar_1992@outlook.com>
2022-07-21 13:42:58 -0500

    cambiando api

--- src/config/AxiosConfig.js
+++ src/config/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
     const localApi = 'http://localhost:8080/api';
     const prodApi = 'https://api-sistema-venta.herokuapp.com/api';
 export const axiosConfigLocal = axios.create({
-    baseURL:localApi
+    baseURL:prodApi
 
 });
 
